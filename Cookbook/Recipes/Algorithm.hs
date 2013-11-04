ee6027dc3def4fcc5807fbd2b552faa8e78ab752
Nate Pisarski <nathanpisarski@gmail.com>
2013-11-04 12:00:11 -0500

    Add more algorithms

--- Cookbook/Recipes/Algorithm.hs
+++ Cookbook/Recipes/Algorithm.hs
@@ -5,12 +5,25 @@ module Cookbook.Recipes.Algorithm(climb,genMatrix) where
 import Cookbook.Recipes.DataStructures
 import Cookbook.Ingredients.Lists.Modify
 
-climb :: (Tree a) -> a -> [a]
-climb x c = case x of (Empty) -> [];
-                      (Branch a (Empty) b) -> a : climb b c
-                      (Branch a b (Empty)) -> a : climb b c
-                      (Branch a b d) -> a : climb b c ++ climb d c
+-- | Gets all the nodes in a binary tree, putting them in a list.
+climb :: (Tree a) -> [a]
+climb x = case x of (Empty) -> [];(Branch a (Empty) b) -> a : climb b;
+                    (Branch a b (Empty)) -> a : climb b;
+                    (Branch a b d) -> a : climb b ++ climb d 
 
+-- | Provides map-like behavior for binary trees.
+treeMap :: (Tree a) -> (a -> a) -> (Tree a)
+treeMap tr f = case tr of Empty -> Empty;
+                          (Branch a (Empty) z@(Branch c _ _)) -> (Branch (f c) (treeMap z f) (Empty));
+                          (Branch a z@(Branch b _ _) (Empty)) -> (Branch (f b) (Empty) (treeMap z f))
+
+-- | Provides filter-like behavior for binary trees.
+treeFilter :: (Tree a) -> (a -> Bool) -> (Tree a)
+treeFilter Empty f = Empty
+treeFilter (Branch a (Empty) z@(Branch b _ _)) f = if (f a) then (Branch a (Empty) (treeFilter z f)) else Empty
+treeFilter (Branch a z@(Branch b _ _) (Empty)) f = if (f a) then (Branch a (treeFilter z f) (Empty)) else Empty
+
+-- | Generates a matrix (coord1,coord2,a) with the specified data to null it out.
 genMatrix :: (Int,Int) -> a -> [((Int,Int),a)]
 genMatrix (a,b) c = helper (0,0)
   where helper (d,e)
--- Cookbook/Recipes/DataStructures.hs
+++ Cookbook/Recipes/DataStructures.hs
@@ -2,6 +2,7 @@
 --Higher-level data structures for use in the rest of Cookbook.
 module Cookbook.Recipes.DataStructures(Tree(..),) where
 
+-- | Cookie-cutter implementation of a binary tree.
 data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show)--add a "map" and "filter" function for Tree.
 
 
