379e55dbfb70ad6937a9b687d19f214ffc2e3be9
Nathan Pisarski <nate@im>
2014-06-20 15:02:38 -0400

    Fix mixing module

--- Cookbook/Recipes/Meta.hs
+++ Cookbook/Recipes/Meta.hs
@@ -2,12 +2,10 @@ module Cookbook.Recipes.Meta(module Cookbook.Recipes.Algorithm,
                              module Cookbook.Recipes.Cline,
                              module Cookbook.Recipes.DataStructures,
                              module Cookbook.Recipes.Math,
-                             module Cookbook.Recipes.Sanitize,
-                             module Cookbook.Recipes.WordStats) where
+                             module Cookbook.Recipes.Sanitize) where
 
 import Cookbook.Recipes.Algorithm
 import Cookbook.Recipes.Cline
 import Cookbook.Recipes.DataStructures
 import Cookbook.Recipes.Math
 import Cookbook.Recipes.Sanitize
-import Cookbook.Recipes.WordStats
