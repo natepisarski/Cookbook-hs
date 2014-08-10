ce1a8ec0c99c589d5f87b575ac1d92e8a684d2c2
Nathaniel Pisarski <nathanpisarski@gmail.com>
2014-08-10 12:18:39 -0400

    Make this project build again.

--- dist/build/autogen/Paths_cookbook.hs
+++ dist/build/autogen/Paths_cookbook.hs
@@ -1,7 +1,7 @@
 module Paths_cookbook (
     version,
     getBinDir, getLibDir, getDataDir, getLibexecDir,
-    getDataFileName
+    getDataFileName, getSysconfDir
   ) where
 
 import qualified Control.Exception as Exception
@@ -15,18 +15,20 @@ catchIO = Exception.catch
 
 version :: Version
 version = Version {versionBranch = [3,0,0,0], versionTags = []}
-bindir, libdir, datadir, libexecdir :: FilePath
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
 
 bindir     = "/home/nate/.cabal/bin"
-libdir     = "/home/nate/.cabal/lib/cookbook-3.0.0.0/ghc-7.6.3"
-datadir    = "/home/nate/.cabal/share/cookbook-3.0.0.0"
+libdir     = "/home/nate/.cabal/lib/x86_64-linux-ghc-7.8.3/cookbook-3.0.0.0"
+datadir    = "/home/nate/.cabal/share/x86_64-linux-ghc-7.8.3/cookbook-3.0.0.0"
 libexecdir = "/home/nate/.cabal/libexec"
+sysconfdir = "/home/nate/.cabal/etc"
 
-getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "cookbook_bindir") (\_ -> return bindir)
 getLibDir = catchIO (getEnv "cookbook_libdir") (\_ -> return libdir)
 getDataDir = catchIO (getEnv "cookbook_datadir") (\_ -> return datadir)
 getLibexecDir = catchIO (getEnv "cookbook_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "cookbook_sysconfdir") (\_ -> return sysconfdir)
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName name = do
